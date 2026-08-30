87391e31be72aca0f9ece1beb198b71307f1ab72
Olaoluwa <olaoluwalawal011@gmail.com>
2026-08-30 23:02:26 +0100

    added the render link

--- client/src/Services/api.js
+++ client/src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://gigflow-qc81.onrender.com/api",
   headers: {
     "Content-Type": "application/json"
   }
