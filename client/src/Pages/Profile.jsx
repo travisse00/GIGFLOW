da8bef8a006423a882ecc63261b7e9e87e16f759
Olaoluwa <olaoluwalawal011@gmail.com>
2026-08-31 14:31:10 +0100

    profile edit

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -252,6 +252,12 @@ function Profile() {
                 </div>
 
               </div>
+              <Link
+  to="/edit-profile"
+  className="rounded-md bg-violet-600 px-4 py-2 text-sm font-medium transition hover:bg-violet-700"
+>
+  Edit Profile
+</Link>
 
             </div>
 
